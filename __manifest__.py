c958fc2525cbd3e9d0c722ecb8233e1c83de7aea
Arman Nur Hidayat <armannurhidayat7@gmail.com>
2020-05-22 15:13:16 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vit_custom_report_saleorder",
+
+    'summary': """
+        Custom report saleorder
+    """,
+
+    'description': """
+        Custom report saleorder
+    """,
+
+    'author': "Arman Nur Hidayat <Vitraining>",
+    'website': "http://www.vitraining.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'report/views.xml',
+    ],
+}
